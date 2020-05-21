fc435ee98f8c4c8d02a56e631c0320bd8a6122b5
Rduque2020 <65695742+Rduque2020@users.noreply.github.com>
2020-05-20 22:27:05 -0500

    Update RC_PART_2_CODE_4_RELAYS.ino

--- RC_PART_2_CODE_4_RELAYS.ino
+++ RC_PART_2_CODE_4_RELAYS.ino
@@ -180,3 +180,32 @@ int calc_dist_L() {
 
     return dis_L;
 }
+
+
+const int trigPin = 11
+const int echoPin = 10;
+const int in1 = 9;
+const int in2 = 8;
+const int in3 = 4;
+const int in4 = 3;
+
+void setup()
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode (in1, OUTPUT); 
+  PinMode (in2, OUPUT);
+  pinMode (in3, OUTPUT);
+  pinMode (in4, OUTPUT);
+
+void loop()
+{
+  digitalWrite (trigPin, LOW);
+  delayMicroseconds (?); 
+  digital Write (trigPin, High);
+  delatyMicroseconds (?);
+  digitalWrite (trigPin, LOW);
+  duration = pulseIn (echoPin, HIGH); 
+  distance = duration/?;
+  if(distance<30)
+    
